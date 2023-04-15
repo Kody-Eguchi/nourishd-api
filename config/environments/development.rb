97677e5c2214239ff8253709d6699a4eff61f48b
Koji Eguchi <koji.eguchi@hotmail.com>
2023-04-15 18:05:24 -0400

    Fix cross-domain cookie issue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,4 +63,15 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+
+  config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'http://localhost:3002/' # Frontend origin
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: true
+    end
+  end
 end
