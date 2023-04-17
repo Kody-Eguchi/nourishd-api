f62751aa4b587e37b6c6cbea7486972615ea5535
Koji Eguchi <koji.eguchi@hotmail.com>
2023-04-17 09:46:15 -0400

    Refactored codes for cross-domain middleware config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
 
   config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins `http://localhost:#{ENV['PORT']}/` # Frontend origin
+      origins "http://localhost:#{ENV['PORT']}/" # Frontend origin
       resource '*',
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head],
