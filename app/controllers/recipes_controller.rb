af9dae1e6d62f32f990255e488d42b80cdcd0641
Koji Eguchi <koji.eguchi@hotmail.com>
2023-04-19 11:04:13 -0400

    Add query action to search by recipe ID

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,6 +9,27 @@ class RecipesController < ApplicationController
 
   def show
     response = EdamomApi.get(params[:id])
+
+  
+    parsed_response = JSON.parse(response)
+    if params['recipe_id']
+     
+      hits = parsed_response['hits']
+    
+      hits.each do |hit|
+        recipe = hit['recipe']
+        puts '🍋'
+        puts recipe
+        puts '🍋'
+        if recipe['uri'].include?(params['recipe_id'])
+          return render json: recipe
+        end
+      end
+    end
     render json: response
   end
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   get '/userInfo' => 'users#getUserById'
   get '/dayInfo' => 'days#getDayById'
   post '/updateDayInfo' => 'days#updateDayInfo'
+  get '/recipeSearch' => 'recipes#getRecipeById'
 end
