ba2ebfa9dde14036aadd64398bf93e378688625a
Koji Eguchi <koji.eguchi@hotmail.com>
2023-05-02 18:57:25 -0400

    Change config

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,2 @@
 # Rails.application.config.session_store :cookie_store, key: '_nutrition_app_api_session', domain: :all, tld_length: 2
-Rails.application.config.session_store :cookie_store, {
-  key: '_nutrition_app_api_session',
-  domain: :all,
-  same_site: :none,
-  secure: Rails.env.production?,
-  tld_length: 2
-}
+Rails.application.config.session_store :cookie_store, key: '_nutrition_app_api_session', domain: :all, same_site: :none, secure: Rails.env.production?
