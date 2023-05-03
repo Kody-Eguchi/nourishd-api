9b771e1a6c33b7b288cb81ae46c3716833192b6d
Koji Eguchi <koji.eguchi@hotmail.com>
2023-05-02 21:16:56 -0400

    Fix an error for safari browser

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,2 @@
 # Rails.application.config.session_store :cookie_store, key: '_nutrition_app_api_session', domain: :all, tld_length: 2
-Rails.application.config.session_store :cookie_store, key: '_nutrition_app_api_session', domain: :all, same_site: :none, secure: Rails.env.production?
+Rails.application.config.session_store :cookie_store, key: '_nutrition_app_api_session', domain: :all, same_site: :none, secure: true
